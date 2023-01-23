adadb822ae0ac5010813383425460f2136e9d093
gdicera <giuseppedicera@uchicago.edu>
2023-01-23 23:49:04 +0000

    made progress on random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   def blank_square_form
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
@@ -9,10 +10,13 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
+  def blank_random_form
+    render({ :template => "calculation_templates/rand_form.html.erb"})
+  end
+
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
-
     render({ :template => "calculation_templates/rand_results.html.erb" })
   end
 end
