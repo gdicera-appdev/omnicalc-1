017141a606e3d666ff71a3d499b805bc9831a606
gdicera <giuseppedicera@uchicago.edu>
2023-01-24 03:13:48 +0000

    finished random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,34 @@ class ApplicationController < ActionController::Base
   end
 
   def blank_random_form
-    render({ :template => "calculation_templates/rand_form.html.erb"})
+    render({ :template => "calculation_templates/rand_form.html.erb" })
   end
 
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
     render({ :template => "calculation_templates/rand_results.html.erb" })
   end
+
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @num = params.fetch("wolf").to_f
+    @square_root = Math.sqrt(@num)
+    render({ :template => "calculation_templates/square_root_results.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    @num = params.fetch("wolf").to_f
+    @square_root = Math.sqrt(@num)
+    render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
+
 end
